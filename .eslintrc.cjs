8b9fea5f17637523e50433780e093ec82416e801
Данил <d.zavgorodnij@meta.studio>
2024-02-15 19:59:31 +0700

    build: install storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
 		'eslint:recommended',
 		'@vue/eslint-config-typescript',
 		'@vue/eslint-config-prettier/skip-formatting',
+		'plugin:storybook/recommended',
 	],
 	parserOptions: {
 		ecmaVersion: 'latest',
