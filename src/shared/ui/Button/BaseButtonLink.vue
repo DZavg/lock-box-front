83ae4432404d93582f408f39013ed06904576ebe
Данил <d.zavgorodnij@meta.studio>
2024-02-19 19:59:15 +0700

    feat(BaseButtonLink): create component and story

---
+++ src/shared/ui/Button/BaseButtonLink.vue
@@ -0,0 +1,17 @@
+<template>
+	<RouterLink :to="link" class="button-link">
+		<slot></slot>
+	</RouterLink>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	link: string | object
+}
+
+withDefaults(defineProps<Props>(), {
+	link: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
