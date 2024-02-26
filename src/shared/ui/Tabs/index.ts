e5b97cc5dbe7c8ba7546a48abfd3c00dff058eeb
Данил <d.zavgorodnij@meta.studio>
2024-02-26 20:03:28 +0700

    feat(Tabs): add export

---
+++ src/shared/ui/Tabs/index.ts
@@ -0,0 +1,5 @@
+import BaseTabs from '@/shared/ui/Tabs/BaseTabs.vue'
+import BaseTabContent from '@/shared/ui/Tabs/BaseTabContent.vue'
+import BaseTabButton from '@/shared/ui/Tabs/BaseTabButton.vue'
+
+export { BaseTabs, BaseTabContent, BaseTabButton }
