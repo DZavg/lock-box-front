f4f1744a03a02c87c2262f97bf447c84031341e5
Данил <d.zavgorodnij@meta.studio>
2024-02-26 21:53:24 +0700

    feat(BaseButton): create BaseButtonWithLoading, ButtonLoading components, add styles and emit

--- src/shared/stories/ui/Button/BaseButton.stories.ts
+++ src/shared/stories/ui/Button/BaseButton.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { BaseButton } from '@/shared/ui/Button/index'
+import { ButtonBackground } from '@/shared/types/Button/ButtonBackground'
+import BaseButton from '@/shared/ui/Button/BaseButton.vue'
 
 const meta: Meta<typeof BaseButton> = {
 	component: BaseButton,
@@ -7,7 +8,7 @@ const meta: Meta<typeof BaseButton> = {
 	parameters: {
 		design: {
 			type: 'figma',
-			url: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=20-4880&mode=design&t=xtqnNljh5dqIFBuN-0',
 		},
 	},
 	argTypes: {
@@ -15,16 +16,38 @@ const meta: Meta<typeof BaseButton> = {
 			control: 'radio',
 			options: ['submit', 'reset', 'button'],
 		},
+		background: {
+			control: 'radio',
+			options: ButtonBackground,
+		},
 	},
 }
 
 export default meta
 type Story = StoryObj<typeof BaseButton>
 
-export const Default: Story = {
+export const Fill: Story = {
 	args: {
 		disabled: false,
 		type: 'button',
+		background: ButtonBackground.Fill,
+	},
+	render: (args: any) => ({
+		components: { BaseButton },
+		setup() {
+			return { args }
+		},
+		template: `
+			<BaseButton v-bind="args">Скопировать пароль</BaseButton>
+		`,
+	}),
+}
+
+export const Border: Story = {
+	args: {
+		disabled: false,
+		type: 'button',
+		background: ButtonBackground.Border,
 	},
 	render: (args: any) => ({
 		components: { BaseButton },
---
+++ src/shared/types/Button/ButtonBackground.ts
@@ -0,0 +1,4 @@
+export enum ButtonBackground {
+	Fill = 'fill',
+	Border = 'border',
+}
