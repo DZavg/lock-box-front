10a68efa1a3935c1bce231ec240c27b70cd081de
Данил <d.zavgorodnij@meta.studio>
2024-02-26 21:54:47 +0700

    feat(BaseTabs): remove index export

--- src/shared/ui/Tabs/index.ts
+++
@@ -1,5 +0,0 @@
-import BaseTabs from '@/shared/ui/Tabs/BaseTabs.vue'
-import BaseTabContent from '@/shared/ui/Tabs/BaseTabContent.vue'
-import BaseTabButton from '@/shared/ui/Tabs/BaseTabButton.vue'
-
-export { BaseTabs, BaseTabContent, BaseTabButton }
