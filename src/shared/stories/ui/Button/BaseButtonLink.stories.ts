9f3b7d2282f80d3e3b92c43dbe9fe7429c4c905e
Данил <d.zavgorodnij@meta.studio>
2024-02-26 21:54:19 +0700

    feat(BaseButton): remove index export

--- src/shared/stories/ui/Button/BaseButtonLink.stories.ts
+++ src/shared/stories/ui/Button/BaseButtonLink.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { BaseButtonLink } from '@/shared/ui/Button/index'
+import BaseButtonLink from '@/shared/ui/Button/BaseButtonLink.vue'
 
 const meta: Meta<typeof BaseButtonLink> = {
 	component: BaseButtonLink,
--- src/shared/ui/Button/index.ts
+++
@@ -1,4 +0,0 @@
-import BaseButton from '@/shared/ui/Button/BaseButton.vue'
-import BaseButtonLink from '@/shared/ui/Button/BaseButtonLink.vue'
-
-export { BaseButton, BaseButtonLink }
