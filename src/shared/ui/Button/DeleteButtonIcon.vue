ed094b5f52d0fcc5cd8f4425dd4f0d47685f9710
Данил <d.zavgorodnij@meta.studio>
2024-02-27 20:55:07 +0700

    feat(DeleteButtonIcon): create component and story

---
+++ src/shared/ui/Button/DeleteButtonIcon.vue
@@ -0,0 +1,10 @@
+<template>
+	<BaseButtonIcon :icon="iconDelete" />
+</template>
+
+<script lang="ts" setup>
+import iconDelete from '@/shared/images/svg/icon-delete.svg'
+import BaseButtonIcon from '@/shared/ui/Button/BaseButtonIcon.vue'
+</script>
+
+<style lang="scss" scoped></style>
