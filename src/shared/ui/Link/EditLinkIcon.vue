def551626dc90682be10a77ea0ed95a0eacb9f99
Данил <d.zavgorodnij@meta.studio>
2024-02-27 21:07:31 +0700

    feat(EditLinkIcon): create component and story

---
+++ src/shared/ui/Link/EditLinkIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<BaseLinkIcon :icon="iconEdit" :link="link" />
+</template>
+
+<script lang="ts" setup>
+import iconEdit from '@/shared/images/svg/icon-edit.svg'
+import BaseLinkIcon from '@/shared/ui/Link/BaseLinkIcon.vue'
+
+interface Props {
+	link: string | object
+}
+
+withDefaults(defineProps<Props>(), {
+	link: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
