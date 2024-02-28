0a4dd112c92f32a94f5595c95f3a0bd14a9db1b0
Данил <d.zavgorodnij@meta.studio>
2024-02-28 22:17:53 +0700

    feat(SearchInput): create component and story

---
+++ src/shared/stories/ui/Input/SearchInput.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import SearchInput from '@/shared/ui/Input/SearchInput.vue'
+
+const meta: Meta<typeof SearchInput> = {
+	component: SearchInput,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=20-3697&mode=design&t=gx3S1080J5P0Gl8k-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof SearchInput>
+
+export const Default: Story = {}
---
+++ src/shared/ui/Input/SearchInput.vue
@@ -0,0 +1,50 @@
+<template>
+	<BaseInput class="search-input" v-bind="props">
+		<BaseIcon class="search-input__icon" :icon="iconSearch" :color="iconColor" :size="16" />
+	</BaseInput>
+</template>
+
+<script lang="ts" setup>
+import BaseInput from '@/shared/ui/Input/BaseInput.vue'
+import BaseIcon from '@/shared/ui/Icon/BaseIcon.vue'
+import iconSearch from '@/shared/images/svg/icon-search.svg'
+import { computed } from 'vue'
+import { IconColor } from '@/shared/types/Icon/IconColor'
+
+interface Props {
+	name?: string
+	modelValue?: string
+	placeholder?: string
+	disabled?: boolean
+	autocomplete?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	name: 'search',
+	modelValue: '',
+	placeholder: 'Поиск',
+	disabled: false,
+	autocomplete: 'off',
+})
+
+const iconColor = computed(() => (props.disabled ? IconColor.GrayThird : IconColor.White))
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+	&:deep(.input__wrapper) {
+		position: relative;
+
+		input {
+			padding-right: 44px;
+		}
+	}
+
+	&__icon {
+		position: absolute;
+		right: $indent-s;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+}
+</style>
