a878b13c226005456e8605bc116c71ef203018c1
Данил <d.zavgorodnij@meta.studio>
2024-02-28 20:37:27 +0700

    feat(PasswordInput): create component and story

---
+++ src/shared/ui/Input/PasswordInput.vue
@@ -0,0 +1,58 @@
+<template>
+	<BaseInput class="password-input" v-bind="props" :type="type">
+		<EyeButtonIcon class="password-input__button" @click="switchType" :active="active" />
+	</BaseInput>
+</template>
+
+<script lang="ts" setup>
+import BaseInput from '@/shared/ui/Input/BaseInput.vue'
+import EyeButtonIcon from '@/shared/ui/Button/EyeButtonIcon.vue'
+import { computed, ref, type Ref } from 'vue'
+
+interface Props {
+	name: string
+	modelValue?: string
+	label: string
+	placeholder: string
+	error?: string
+	disabled?: boolean
+	autocomplete?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	name: '',
+	modelValue: '',
+	label: '',
+	placeholder: '',
+	error: '',
+	disabled: false,
+	autocomplete: 'off',
+})
+
+const active: Ref<boolean> = ref(false)
+
+const type = computed(() => (active.value ? 'text' : 'password'))
+
+const switchType = () => {
+	active.value = !active.value
+}
+</script>
+
+<style lang="scss" scoped>
+.password-input {
+	&:deep(.input__wrapper) {
+		position: relative;
+
+		input {
+			padding-right: 44px;
+		}
+	}
+
+	&__button {
+		position: absolute;
+		right: $indent-s;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+}
+</style>
