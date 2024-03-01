66d3d07dcf75a1d6dbf85668f731a5310ed473b4
Данил <d.zavgorodnij@meta.studio>
2024-03-01 19:12:43 +0700

    feat(BaseRadio): create component and story

---
+++ src/shared/ui/Radio/BaseRadio.vue
@@ -0,0 +1,88 @@
+<template>
+	<label
+		class="radio"
+		:class="{
+			['radio--disabled']: disabled,
+		}"
+	>
+		<input
+			v-model="inputValue"
+			type="radio"
+			:value="value"
+			:name="name"
+			:disabled="disabled"
+			hidden
+		/>
+		<span class="radio__circle">
+			<span class="radio__point"></span>
+		</span>
+		<span class="radio__title">
+			<slot></slot>
+		</span>
+	</label>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	name: string
+	modelValue: string
+	value?: string
+	disabled?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+	name: '',
+	value: '',
+	modelValue: '',
+	disabled: false,
+})
+
+defineEmits<{
+	(e: 'update:modelValue', value: string): void
+}>()
+
+const inputValue = defineModel('modelValue', { default: '' })
+</script>
+
+<style lang="scss" scoped>
+.radio {
+	display: flex;
+	align-items: center;
+	gap: $indent-xs;
+	user-select: none;
+	cursor: pointer;
+	color: $color-white;
+
+	input:checked + .radio__circle {
+		border-color: $color-blue-primary;
+
+		.radio__point {
+			background-color: $color-blue-primary;
+		}
+	}
+
+	&--disabled {
+		cursor: default;
+		pointer-events: none;
+	}
+
+	&__circle {
+		flex-shrink: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 16px;
+		height: 16px;
+		border-radius: 50%;
+		border: 1px solid $color-white;
+		transition: border-color 0.1s ease-in;
+	}
+
+	&__point {
+		width: 10px;
+		height: 10px;
+		border-radius: 50%;
+		transition: background-color 0.1s ease-in;
+	}
+}
+</style>
