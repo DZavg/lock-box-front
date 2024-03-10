489e078749933f9ccf29698d81212b5d5eff3a6b
Данил <d.zavgorodnij@meta.studio>
2024-03-10 14:36:01 +0700

    feat(DropdownButtonIcon): create component and story

---
+++ src/shared/ui/Button/DropdownButtonIcon.vue
@@ -0,0 +1,30 @@
+<template>
+	<BaseButtonIcon
+		class="dropdown-button-icon"
+		:icon="iconArrowSmall"
+		size="16"
+		:icon-color="iconColor"
+	/>
+</template>
+
+<script lang="ts" setup>
+import iconArrowSmall from '@/shared/images/svg/icon-arrow-small.svg'
+import BaseButtonIcon from '@/shared/ui/Button/BaseButtonIcon.vue'
+import { IconColor } from '@/shared/types/Icon/IconColor'
+
+interface Props {
+	iconColor?: IconColor
+}
+
+withDefaults(defineProps<Props>(), {
+	iconColor: IconColor.White,
+})
+</script>
+
+<style lang="scss" scoped>
+.dropdown-button-icon {
+	&:deep(svg) {
+		rotate: 90deg;
+	}
+}
+</style>
