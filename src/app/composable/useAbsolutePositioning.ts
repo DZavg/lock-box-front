9b37073ee4ab6e27d33cc7763fdf0a9bf8d90c2e
Данил <d.zavgorodnij@meta.studio>
2024-03-10 15:19:47 +0700

    feat(composable): create useAbsolutePositioning

---
+++ src/app/composable/useAbsolutePositioning.ts
@@ -0,0 +1,68 @@
+import { onBeforeUnmount, onMounted, type Ref, ref } from 'vue'
+import { Position } from '@/app/types/AbsolutePositioning/Position'
+import throttle from '@/shared/utils/throttle'
+
+export default function (
+	parentElement: Ref<HTMLElement | null>,
+	element: Ref<HTMLElement | null>,
+	defaultPosition: Position,
+	positions: Position[] = [Position.Top, Position.Right, Position.Bottom, Position.Left],
+) {
+	const defaultDOMRect = {
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+	}
+	const side = ref('')
+
+	const checkPosition = () => {
+		const elementDOMRect = parentElement.value?.getBoundingClientRect() || defaultDOMRect
+
+		const elementHeight = element.value?.clientHeight || 0
+		const elementWidth = element.value?.clientWidth || 0
+
+		const windowWidth = window.innerWidth
+		const windowHeight = window.innerHeight
+
+		const elementRect = {
+			top: elementDOMRect.top - elementHeight,
+			right: windowWidth - elementDOMRect.right - elementWidth / 2,
+			bottom: windowHeight - elementDOMRect.bottom - elementHeight,
+			left: elementDOMRect.left - elementWidth / 2,
+		}
+
+		if (elementRect.bottom > 0 && positions.includes(Position.Bottom)) {
+			side.value = Position.Bottom
+			return
+		} else if (elementRect.top > 0 && positions.includes(Position.Top)) {
+			side.value = Position.Top
+			return
+		} else if (elementRect.right > 0 && positions.includes(Position.Right)) {
+			side.value = Position.Right
+			return
+		} else if (elementRect.left > 0 && positions.includes(Position.Left)) {
+			side.value = Position.Left
+			return
+		}
+
+		side.value = defaultPosition
+	}
+
+	const checkPositionWithThrottle = throttle(checkPosition)
+
+	onMounted(() => {
+		checkPosition()
+		window.addEventListener('scroll', checkPositionWithThrottle)
+		window.addEventListener('resize', checkPositionWithThrottle)
+	})
+
+	onBeforeUnmount(() => {
+		window.removeEventListener('scroll', checkPositionWithThrottle)
+		window.removeEventListener('resize', checkPositionWithThrottle)
+	})
+
+	return {
+		side,
+	}
+}
