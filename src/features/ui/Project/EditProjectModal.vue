a2b8a20b3abd72b0a526366eb90d174bb3a236da
Данил <d.zavgorodnij@meta.studio>
2024-03-12 21:06:53 +0700

    feat(EditProjectModal): create component and story

---
+++ src/features/ui/Project/EditProjectModal.vue
@@ -0,0 +1,35 @@
+<template>
+	<BaseModal class="edit-project-modal">
+		<p class="h2 modal-h1-title edit-project-modal__title">Редактировать проект</p>
+		<div class="edit-project-modal__form">
+			<BaseInput label="Название проекта" placeholder="Поле ввода" name="project_name" />
+			<BaseInput label="Домен" placeholder="Поле ввода" name="project_domain" />
+		</div>
+		<BaseButton class="edit-project-modal__button">Сохранить изменения</BaseButton>
+	</BaseModal>
+</template>
+
+<script lang="ts" setup>
+import BaseModal from '@/shared/ui/Modal/BaseModal.vue'
+import BaseInput from '@/shared/ui/Input/BaseInput.vue'
+import BaseButton from '@/shared/ui/Button/BaseButton.vue'
+</script>
+
+<style lang="scss" scoped>
+.edit-project-modal {
+	&__title {
+		margin-bottom: $indent-s;
+	}
+
+	&__form {
+		display: flex;
+		flex-direction: column;
+		gap: $indent-s;
+		margin-bottom: $indent-l;
+	}
+
+	&__button {
+		width: 100%;
+	}
+}
+</style>
