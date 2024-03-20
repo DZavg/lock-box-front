e62d8846b207222f27e7707dc77a01db6e9290e1
Данил <d.zavgorodnij@meta.studio>
2024-03-20 20:29:45 +0700

    feat(BaseDropdown): create component and story

---
+++ src/shared/ui/Dropdown/BaseDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="dropdown">
+		<div class="dropdown__head">
+			<p><slot name="title"></slot></p>
+			<slot name="button" :click="$emit('onClick')"></slot>
+		</div>
+		<div v-if="isActive" class="dropdown__body">
+			<slot name="content"></slot>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	isActive: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+	isActive: false,
+})
+
+defineEmits<{
+	(e: 'onClick'): void
+}>()
+</script>
+
+<style lang="scss" scoped>
+.dropdown {
+	&__head {
+		word-break: break-word;
+	}
+}
+</style>
