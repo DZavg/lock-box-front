bf4cfe21626ee36f8bf2a203b564496ae1203c19
Данил <d.zavgorodnij@meta.studio>
2024-03-25 21:40:08 +0700

    feat(BaseForm): create component and story

---
+++ src/shared/ui/Form/BaseForm.vue
@@ -0,0 +1,13 @@
+<template>
+	<form @submit.prevent="$emit('submit')" @keydown.enter.prevent="$emit('submit')">
+		<slot></slot>
+	</form>
+</template>
+
+<script lang="ts" setup>
+defineEmits<{
+	(e: 'submit'): void
+}>()
+</script>
+
+<style lang="scss" scoped></style>
