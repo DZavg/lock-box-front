69d03f5e84dadda53835d0bab756dcbfd107e245
Данил <d.zavgorodnij@meta.studio>
2024-03-25 22:13:27 +0700

    feat(EmailInput): create component and story

---
+++ src/shared/ui/Input/EmailInput.vue
@@ -0,0 +1,9 @@
+<template>
+	<BaseInput label="Электронная почта" name="email" placeholder="ivan@gmail.com" />
+</template>
+
+<script lang="ts" setup>
+import BaseInput from '@/shared/ui/Input/BaseInput.vue'
+</script>
+
+<style lang="scss" scoped></style>
