7f0b656ef33d35ed9ee940e60995725d93cfab67
Данил <d.zavgorodnij@meta.studio>
2024-03-25 22:08:25 +0700

    feat(AuthCard): create component and story

---
+++ src/entities/stories/Auth/AuthCard.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import AuthCard from '@/entities/ui/Auth/AuthCard.vue'
+
+const meta: Meta<typeof AuthCard> = {
+	component: AuthCard,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=1-2&mode=design&t=6ngpJBBHFj4nsuMb-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof AuthCard>
+
+export const Default: Story = {
+	args: {
+		title: 'Добро пожаловать',
+	},
+}
---
+++ src/entities/ui/Auth/AuthCard.vue
@@ -0,0 +1,20 @@
+<template>
+	<BaseCard class="container-s">
+		<h1 class="h1-title">{{ title }}</h1>
+		<slot></slot>
+	</BaseCard>
+</template>
+
+<script lang="ts" setup>
+import BaseCard from '@/shared/ui/Card/BaseCard.vue'
+
+interface Props {
+	title: string
+}
+
+withDefaults(defineProps<Props>(), {
+	title: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
