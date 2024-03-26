e97b442d4a5545c37cd7f1cfd26d9577e81edcc6
Данил <d.zavgorodnij@meta.studio>
2024-03-26 20:20:49 +0700

    feat(LogoWithDescription): create component and story

---
+++ src/shared/stories/ui/Logo/LogoWithDescription.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import LogoWithDescription from '@/shared/ui/Logo/LogoWithDescription.vue'
+
+const meta: Meta<typeof LogoWithDescription> = {
+	component: LogoWithDescription,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=47-1689&mode=design&t=zJWQjiCQ8vXk6kyc-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof LogoWithDescription>
+
+export const Default: Story = {}
---
+++ src/shared/ui/Logo/LogoWithDescription.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="logo-with-desc">
+		<svg class="logo-with-desc__icon">
+			<use :href="iconLogo + '#icon'" />
+		</svg>
+		<p>Ваш надежный хранитель паролей</p>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import iconLogo from '@/shared/images/svg/icon-logo.svg'
+</script>
+
+<style lang="scss" scoped>
+.logo-with-desc {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: $indent-s;
+
+	@media screen and (max-width: 475px) {
+		gap: $indent-xs;
+	}
+
+	&__icon {
+		width: 197px;
+		height: 32px;
+		fill: $color-white;
+
+		@media screen and (max-width: 475px) {
+			width: 148px;
+			height: 24px;
+		}
+	}
+}
+</style>
