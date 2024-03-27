03b118b71d6ec7b9d9ab6dbfbf77e293ca47680d
Данил <d.zavgorodnij@meta.studio>
2024-03-27 20:06:22 +0700

    feat(RegistrationTabContent): create component and story

---
+++ src/widgets/ui/Auth/RegistrationTabContent.vue
@@ -0,0 +1,35 @@
+<template>
+	<BaseForm class="registration-tab-content">
+		<InputList class="registration-tab-content__inputs">
+			<BaseInput label="Имя" name="name" placeholder="Иванов Иван" />
+			<EmailInput />
+			<PasswordInput />
+			<PasswordInput label="Повтор пароля" name="password_repeat" />
+		</InputList>
+		<BaseButton>Зарегистрироваться</BaseButton>
+	</BaseForm>
+</template>
+
+<script lang="ts" setup>
+import BaseForm from '@/shared/ui/Form/BaseForm.vue'
+import EmailInput from '@/shared/ui/Input/EmailInput.vue'
+import BaseButton from '@/shared/ui/Button/BaseButton.vue'
+import PasswordInput from '@/shared/ui/Input/PasswordInput.vue'
+import BaseInput from '@/shared/ui/Input/BaseInput.vue'
+import InputList from '@/shared/ui/Input/InputList.vue'
+</script>
+
+<style lang="scss" scoped>
+.registration-tab-content {
+	display: flex;
+	flex-direction: column;
+
+	&__inputs {
+		margin-bottom: $indent-xl;
+
+		@media screen and (max-width: 475px) {
+			margin-bottom: $indent-m;
+		}
+	}
+}
+</style>
