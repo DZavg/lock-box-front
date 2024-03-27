b917dad3832d3ea8a1177ee51096928802c480d0
Данил <d.zavgorodnij@meta.studio>
2024-03-27 21:54:02 +0700

    feat(PasswordRecoveryPage): import RecoveryPasswordSuccessMessage component

--- src/pages/PasswordRecoveryPage.vue
+++ src/pages/PasswordRecoveryPage.vue
@@ -1,17 +1,27 @@
 <template>
 	<div class="password-recovery-page container-s">
-		<LogoWithDescription />
-		<RecoveryPasswordForm class="container-s" />
+		<LogoWithDescription v-if="!email" />
+		<RecoveryPasswordForm v-if="!email" @resetPassword="assignEmailValue" />
+		<RecoveryPasswordSuccessMessage v-else :email="email" />
 	</div>
 </template>
 
 <script lang="ts" setup>
 import RecoveryPasswordForm from '@/widgets/ui/RecoveryPassword/RecoveryPasswordForm.vue'
 import LogoWithDescription from '@/shared/ui/Logo/LogoWithDescription.vue'
+import RecoveryPasswordSuccessMessage from '@/widgets/ui/RecoveryPassword/RecoveryPasswordSuccessMessage.vue'
+import { ref, type Ref } from 'vue'
+
+const email: Ref<string> = ref('')
+
+const assignEmailValue = (value: string) => {
+	email.value = value
+}
 </script>
 
 <style lang="scss" scoped>
 .password-recovery-page {
+	width: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
