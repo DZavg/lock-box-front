1b582455fa01b675b6a0c26fde82ffd0f4b27bd1
Данил <d.zavgorodnij@meta.studio>
2024-03-27 21:53:29 +0700

    feat(RecoveryPasswordForm): add emit with email

--- src/widgets/ui/RecoveryPassword/RecoveryPasswordForm.vue
+++ src/widgets/ui/RecoveryPassword/RecoveryPasswordForm.vue
@@ -1,8 +1,8 @@
 <template>
 	<AuthCard class="recovery-password-form" title="Восстановление пароля">
 		<BaseForm class="recovery-password-form__wrapper">
-			<EmailInput />
-			<BaseButton>Сбросить пароль</BaseButton>
+			<EmailInput v-model="form.email" />
+			<BaseButton @click="resetPassword">Сбросить пароль</BaseButton>
 		</BaseForm>
 	</AuthCard>
 </template>
@@ -12,10 +12,25 @@ import AuthCard from '@/entities/ui/Auth/AuthCard.vue'
 import BaseForm from '@/shared/ui/Form/BaseForm.vue'
 import EmailInput from '@/shared/ui/Input/EmailInput.vue'
 import BaseButton from '@/shared/ui/Button/BaseButton.vue'
+import { ref } from 'vue'
+
+const form = ref({
+	email: '',
+})
+
+const emits = defineEmits<{
+	(e: 'resetPassword', email: string): void
+}>()
+
+const resetPassword = () => {
+	emits('resetPassword', form.value.email)
+}
 </script>
 
 <style lang="scss" scoped>
 .recovery-password-form {
+	width: 100%;
+
 	&__wrapper {
 		display: flex;
 		flex-direction: column;
