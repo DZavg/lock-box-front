a797e548a6f06f98a733b06a2d5c2a3cc26b315c
Данил <d.zavgorodnij@meta.studio>
2024-03-27 20:00:35 +0700

    feat(InputList): create component and story

---
+++ src/shared/ui/Input/InputList.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="input-list">
+		<slot></slot>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.input-list {
+	display: flex;
+	flex-direction: column;
+	gap: $indent-s;
+
+	@media screen and (max-width: 475px) {
+		gap: $indent-xs;
+	}
+}
+</style>
