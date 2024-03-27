ec1bbb83520adae86801f833eba607ec6a0af53b
Данил <d.zavgorodnij@meta.studio>
2024-03-27 20:08:59 +0700

    feat(AuthPage): create component and route

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuthLayout from '@/app/layouts/AuthLayout.vue'
 import PasswordRecoveryPage from '@/pages/PasswordRecoveryPage.vue'
+import AuthPage from '@/pages/AuthPage.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +11,13 @@ const router = createRouter({
 			component: AuthLayout,
 			children: [
 				{
+					path: '/auth',
+					name: 'Auth',
+					component: AuthPage,
+				},
+				{
 					path: '/recovery-password',
-					name: 'recovery-password',
+					name: 'RecoveryPassword',
 					component: PasswordRecoveryPage,
 				},
 			],
---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="auth-page">
+		<LogoWithDescription />
+		<AuthTabs />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import LogoWithDescription from '@/shared/ui/Logo/LogoWithDescription.vue'
+import AuthTabs from '@/widgets/ui/Auth/AuthTabs.vue'
+</script>
+
+<style lang="scss" scoped>
+.auth-page {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: $indent-l;
+}
+</style>
