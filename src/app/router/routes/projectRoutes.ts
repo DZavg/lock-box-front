8bf791489213b56e9b98239c84e12df1b7d1c953
Данил <d.zavgorodnij@meta.studio>
2024-04-01 20:46:07 +0700

    feat(Projects): create page

---
+++ src/app/router/routes/projectRoutes.ts
@@ -0,0 +1,15 @@
+import DefaultLayout from '@/app/layouts/DefaultLayout.vue'
+import { RouteName } from '@/app/router/RouteName'
+import ProjectsPage from '@/pages/Projects/ui/ProjectsPage.vue'
+
+export const projectRoutes = {
+	path: '/projects',
+	component: DefaultLayout,
+	children: [
+		{
+			path: '',
+			name: RouteName.Projects,
+			component: ProjectsPage,
+		},
+	],
+}
---
+++ src/pages/Projects/ui/ProjectsPage.vue
@@ -0,0 +1,7 @@
+<template>
+	<p>empty content</p>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
