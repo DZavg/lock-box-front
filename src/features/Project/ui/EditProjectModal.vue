68b24d8ee9c55d8f942c9d32b7e42dfe82fa70c6
Данил <d.zavgorodnij@meta.studio>
2024-04-01 18:52:34 +0700

    refactor(EditProjectModal): use BaseForm and InputList components

--- src/features/Project/ui/EditProjectModal.vue
+++ src/features/Project/ui/EditProjectModal.vue
@@ -1,10 +1,12 @@
 <template>
 	<BaseModal class="edit-project-modal">
 		<p class="h2 modal-h1-title edit-project-modal__title">Редактировать проект</p>
-		<div class="edit-project-modal__form">
-			<BaseInput label="Название проекта" placeholder="Поле ввода" name="project_name" />
-			<BaseInput label="Домен" placeholder="Поле ввода" name="project_domain" />
-		</div>
+		<BaseForm class="edit-project-modal__form">
+			<InputList>
+				<BaseInput label="Название проекта" name="project_name" placeholder="Поле ввода" />
+				<BaseInput label="Домен" name="project_domain" placeholder="Поле ввода" />
+			</InputList>
+		</BaseForm>
 		<BaseButton class="edit-project-modal__button">Сохранить изменения</BaseButton>
 	</BaseModal>
 </template>
@@ -13,18 +15,13 @@
 import BaseModal from '@/shared/ui/Modal/BaseModal.vue'
 import BaseInput from '@/shared/ui/Input/BaseInput.vue'
 import BaseButton from '@/shared/ui/Button/BaseButton.vue'
+import BaseForm from '@/shared/ui/Form/BaseForm.vue'
+import InputList from '@/shared/ui/Input/InputList.vue'
 </script>
 
 <style lang="scss" scoped>
 .edit-project-modal {
-	&__title {
-		margin-bottom: $indent-s;
-	}
-
 	&__form {
-		display: flex;
-		flex-direction: column;
-		gap: $indent-s;
 		margin-bottom: $indent-l;
 	}
 
