f12ca1844867d2783fd877bb2e3630ff9e394d8a
DZavg <Danilkaqaz123@gmail.com>
2024-04-01 21:29:48 +0700

    feat(ProjectTable): add variables with mock data

---
+++ src/widgets/Projects/data/ProjectTable.ts
@@ -0,0 +1,56 @@
+import { type Project } from '@/widgets/Projects/types/Project'
+
+export const projectTableHead = ['Название проекта', 'Домен', '']
+
+export const projectTableBody: Project[] = [
+	{
+		id: '0',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '1',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '2',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '3',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '4',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '5',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '6',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '7',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '8',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+	{
+		id: '9',
+		title: 'Название проекта',
+		domain: 'https://music.yandex.ru',
+	},
+]
