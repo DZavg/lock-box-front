a0f92e0893142aa21210391b55a0014acd780121
Данил <d.zavgorodnij@meta.studio>
2024-04-01 18:51:51 +0700

    refactor(EditAccessModal): use BaseForm and InputList components

--- src/features/Access/ui/EditAccessModal.vue
+++ src/features/Access/ui/EditAccessModal.vue
@@ -1,12 +1,14 @@
 <template>
 	<BaseModal class="edit-access-modal">
-		<p class="h2 modal-h1-title edit-access-modal__title">Редактировать доступ</p>
-		<div class="edit-access-modal__form">
-			<BaseInput label="Адрес" placeholder="Поле ввода" name="address" />
-			<BaseInput label="Логин" placeholder="Поле ввода" name="login" />
-			<PasswordInput />
-			<BaseSelect label="Тип доступа" name="type" :options="[{ id: '0', title: 'SSH' }]" />
-		</div>
+		<p class="h2 modal-h1-title">Редактировать доступ</p>
+		<BaseForm class="edit-access-modal__form">
+			<InputList>
+				<BaseInput label="Адрес" name="address" placeholder="Поле ввода" />
+				<BaseInput label="Логин" name="login" placeholder="Поле ввода" />
+				<PasswordInput />
+				<BaseSelect :options="[{ id: '0', title: 'SSH' }]" label="Тип доступа" name="type" />
+			</InputList>
+		</BaseForm>
 		<BaseButton class="edit-access-modal__button">Сохранить изменения</BaseButton>
 	</BaseModal>
 </template>
@@ -17,18 +19,13 @@ import BaseInput from '@/shared/ui/Input/BaseInput.vue'
 import BaseButton from '@/shared/ui/Button/BaseButton.vue'
 import PasswordInput from '@/shared/ui/Input/PasswordInput.vue'
 import BaseSelect from '@/shared/ui/Select/BaseSelect.vue'
+import BaseForm from '@/shared/ui/Form/BaseForm.vue'
+import InputList from '@/shared/ui/Input/InputList.vue'
 </script>
 
 <style lang="scss" scoped>
 .edit-access-modal {
-	&__title {
-		margin-bottom: $indent-s;
-	}
-
 	&__form {
-		display: flex;
-		flex-direction: column;
-		gap: $indent-s;
 		margin-bottom: $indent-l;
 	}
 
