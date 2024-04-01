23be7037a7569d2b3e92866843594478f61cc3b8
Данил <d.zavgorodnij@meta.studio>
2024-04-01 20:46:07 +0700

    feat(Projects): create page

--- src/app/router/RouteName.ts
+++ src/app/router/RouteName.ts
@@ -1,4 +1,5 @@
 export enum RouteName {
 	Auth = 'auth',
 	RecoveryPassword = 'recovery-password',
+	Projects = 'projects',
 }
--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authRoutes } from '@/app/router/routes/authRoutes'
+import { projectRoutes } from '@/app/router/routes/projectRoutes'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [authRoutes],
+	routes: [authRoutes, projectRoutes],
 })
 
 export default router
