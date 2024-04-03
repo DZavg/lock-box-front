b3b683dc00e140aa6c605c9fa6c910b03ecf2268
DZavg <Danilkaqaz123@gmail.com>
2024-04-03 19:52:54 +0700

    feat(EyeLinkIcon): create component and story

---
+++ src/shared/stories/ui/Link/EyeLinkIcon.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import EyeLinkIcon from '@/shared/ui/Link/EyeLinkIcon.vue'
+
+const meta: Meta<typeof EyeLinkIcon> = {
+	component: EyeLinkIcon,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=1-5&mode=design&t=D2Io8DLOR9CYljSr-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof EyeLinkIcon>
+
+export const Default: Story = {
+	args: {
+		link: '/',
+	},
+}
---
+++ src/shared/ui/Link/EyeLinkIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<BaseLinkIcon :icon="iconEyeOpen" :link="link" />
+</template>
+
+<script lang="ts" setup>
+import iconEyeOpen from '@/shared/images/svg/icon-eye-open.svg'
+import BaseLinkIcon from '@/shared/ui/Link/BaseLinkIcon.vue'
+
+interface Props {
+	link: string | object
+}
+
+withDefaults(defineProps<Props>(), {
+	link: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
