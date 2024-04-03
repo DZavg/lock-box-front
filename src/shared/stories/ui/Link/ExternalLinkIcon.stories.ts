611c54f07eec4d6975e8ba1d7e4a369b53192a11
DZavg <Danilkaqaz123@gmail.com>
2024-04-03 19:49:23 +0700

    feat(ExternalLinkIcon): create component and story

---
+++ src/shared/stories/ui/Link/ExternalLinkIcon.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ExternalLinkIcon from '@/shared/ui/Link/ExternalLinkIcon.vue'
+
+const meta: Meta<typeof ExternalLinkIcon> = {
+	component: ExternalLinkIcon,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=1-5&mode=design&t=D2Io8DLOR9CYljSr-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof ExternalLinkIcon>
+
+export const Default: Story = {
+	args: {
+		link: '/',
+	},
+}
---
+++ src/shared/ui/Link/ExternalLinkIcon.vue
@@ -0,0 +1,18 @@
+<template>
+	<BaseLinkIcon :icon="iconExternalLink" :link="link" target="_blank" />
+</template>
+
+<script lang="ts" setup>
+import iconExternalLink from '@/shared/images/svg/icon-external-link.svg'
+import BaseLinkIcon from '@/shared/ui/Link/BaseLinkIcon.vue'
+
+interface Props {
+	link: string | object
+}
+
+withDefaults(defineProps<Props>(), {
+	link: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
