71a2a9976f4509a8e9e5518ca5b3affdef0245e2
DZavg <Danilkaqaz123@gmail.com>
2024-04-03 19:51:24 +0700

    feat(EditButtonIcon): create component and story

---
+++ src/shared/ui/Button/EditButtonIcon.vue
@@ -0,0 +1,10 @@
+<template>
+	<BaseButtonIcon :icon="iconEdit" />
+</template>
+
+<script lang="ts" setup>
+import iconEdit from '@/shared/images/svg/icon-edit.svg'
+import BaseButtonIcon from '@/shared/ui/Button/BaseButtonIcon.vue'
+</script>
+
+<style lang="scss" scoped></style>
