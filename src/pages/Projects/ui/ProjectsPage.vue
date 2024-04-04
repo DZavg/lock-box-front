cb87a7492907fcdfe330bb7c1aaa43db1fa75d3f
DZavg <Danilkaqaz123@gmail.com>
2024-04-04 21:01:10 +0700

    feat(ProjectsPage): add ProjectsTable component

--- src/pages/Projects/ui/ProjectsPage.vue
+++ src/pages/Projects/ui/ProjectsPage.vue
@@ -1,7 +1,10 @@
 <template>
 	<p>empty content</p>
+	<ProjectsTable />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ProjectsTable from '@/widgets/Projects/ui/ProjectsTable.vue'
+</script>
 
 <style lang="scss" scoped></style>
