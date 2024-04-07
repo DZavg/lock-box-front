6e0b1a2ea6db9353c5527054812dec34031824c2
DZavg <Danilkaqaz123@gmail.com>
2024-04-07 14:34:42 +0700

    feat(ProjectsGroup): create component and story

---
+++ src/widgets/project/group/ui/ProjectsGroup.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="projects-group">
+		<ProjectsTable v-if="!isMobile" :projects="projectTableBody" />
+		<ProjectCardList v-else :projects="projectTableBody" />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import ProjectsTable from '@/widgets/project/table/ui/ProjectsTable.vue'
+import { type Project } from '@/widgets/project/table/types/Project'
+import ProjectCardList from '@/widgets/project/card/ui/ProjectCardList.vue'
+import { projectTableBody } from '@/widgets/project/table/data/ProjectTable'
+import useScreen from '@/shared/lib/composable/useScreen'
+
+interface Props {
+	projects: Project[]
+}
+
+withDefaults(defineProps<Props>(), {
+	projects: () => [],
+})
+
+const { isMobile } = useScreen()
+</script>
+
+<style lang="scss" scoped></style>
