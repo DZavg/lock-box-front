99bd28c0895a656ca98709765167f4a2a254b50d
DZavg <Danilkaqaz123@gmail.com>
2024-04-07 13:48:47 +0700

    feat(ProjectsTable): add prop "projects"

--- src/widgets/project/table/ui/ProjectsTable.vue
+++ src/widgets/project/table/ui/ProjectsTable.vue
@@ -1,15 +1,21 @@
 <template>
 	<BaseTable class="projects-table">
 		<ProjectsTableHead />
-		<ProjectsTableBody :projects="projectTableBody" />
+		<ProjectsTableBody :projects="projects" />
 	</BaseTable>
 </template>
 
 <script lang="ts" setup>
 import BaseTable from '@/shared/ui/Table/BaseTable.vue'
-import { projectTableBody } from '@/widgets/project/table/data/ProjectTable'
 import ProjectsTableBody from '@/widgets/project/table/ui/ProjectsTableBody.vue'
 import ProjectsTableHead from '@/widgets/project/table/ui/ProjectsTableHead.vue'
-</script>
+import { type Project } from '@/widgets/project/table/types/Project'
 
-<style lang="scss" scoped></style>
+interface Props {
+	projects: Project[]
+}
+
+withDefaults(defineProps<Props>(), {
+	projects: () => [],
+})
+</script>
