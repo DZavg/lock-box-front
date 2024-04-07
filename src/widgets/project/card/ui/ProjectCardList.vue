4b3ae80c20da8c4b0912a4a75986bb7248d8ac60
DZavg <Danilkaqaz123@gmail.com>
2024-04-07 14:33:55 +0700

    feat(ProjectCardList): create component and story

---
+++ src/widgets/project/card/ui/ProjectCardList.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="project-card-list">
+		<ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+	</div>
+</template>
+
+<script lang="ts" setup>
+import ProjectCard from '@/widgets/project/card/ui/ProjectCard.vue'
+import { type Project } from '@/widgets/project/table/types/Project'
+
+interface Props {
+	projects: Project[]
+}
+
+withDefaults(defineProps<Props>(), {
+	projects: () => [],
+})
+</script>
+
+<style lang="scss" scoped>
+.project-card-list {
+	display: flex;
+	flex-direction: column;
+	gap: $indent-xs;
+}
+</style>
