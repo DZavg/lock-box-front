0bffce00184fc7471cc198bdddf7aa24d433882c
DZavg <Danilkaqaz123@gmail.com>
2024-04-07 13:39:01 +0700

    build: install eslint-plugin-sonarjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,14 @@ module.exports = {
 		browser: true,
 		node: true,
 	},
+	plugins: ['sonarjs'],
 	extends: [
 		'plugin:vue/vue3-essential',
 		'eslint:recommended',
 		'@vue/eslint-config-typescript',
 		'@vue/eslint-config-prettier/skip-formatting',
 		'plugin:storybook/recommended',
+		'plugin:sonarjs/recommended',
 	],
 	parserOptions: {
 		ecmaVersion: 'latest',
