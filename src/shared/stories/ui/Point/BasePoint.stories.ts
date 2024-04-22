dd56ccf0115c026ad034fef3650aaac332a82dcb
DZavg <Danilkaqaz123@gmail.com>
2024-04-22 22:01:04 +0700

    feat(BasePoint): create component and story

---
+++ src/shared/stories/ui/Point/BasePoint.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import BasePoint from '@/shared/ui/BasePoint.vue'
+
+const meta: Meta<typeof BasePoint> = {
+	component: BasePoint,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=20-3748&mode=design&t=u3vNm8cro9yTyK3O-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof BasePoint>
+
+export const Default: Story = {}
---
+++ src/shared/ui/BasePoint.vue
@@ -0,0 +1,21 @@
+<template>
+	<span :style="{ width: size + 'px', height: size + 'px' }" class="point"></span>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	size?: number
+}
+
+withDefaults(defineProps<Props>(), {
+	size: 12,
+})
+</script>
+
+<style lang="scss" scoped>
+.point {
+	display: block;
+	border-radius: 50%;
+	background-color: $color-blue-primary;
+}
+</style>
