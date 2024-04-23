e1591bd52bd5ecec784f0bf2a0b65c141f033662
DZavg <Danilkaqaz123@gmail.com>
2024-04-23 21:19:33 +0700

    feat(ProjectsPage): add html and mock data

--- src/pages/project/ui/ProjectsPage.vue
+++ src/pages/project/ui/ProjectsPage.vue
@@ -1,7 +1,20 @@
 <template>
-	<p>empty content</p>
+	<BaseInternalPage :breadcrumbs="[]" class="page" title-h1="Проекты">
+		<template #head>
+			<ProjectsActions />
+		</template>
+		<template #default>
+			<BaseDataTableGroup :data="getProjects()" :fields="getTableFields()" />
+		</template>
+	</BaseInternalPage>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import BaseInternalPage from '@/shared/ui/InternalPage/BaseInternalPage.vue'
+import BaseDataTableGroup from '@/shared/ui/Table/BaseDataTableGroup.vue'
+import getTableFields from '@/shared/stories/data/table.js'
+import getProjects from '@/shared/stories/data/projects.js'
+import ProjectsActions from '@/features/projects/ui/ProjectsActions.vue'
+</script>
 
 <style lang="scss" scoped></style>
