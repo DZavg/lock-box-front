37e9ebbfafa415cf0ad3fb17eb2a262330ef40c5
DZavg <Danilkaqaz123@gmail.com>
2024-04-25 21:45:43 +0700

    feat(ContentCentering): create component

---
+++ src/shared/ui/Content/ContentCentering.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="content-centering">
+		<slot></slot>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.content-centering {
+	display: flex;
+	flex: 1 0 auto;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+}
+</style>
