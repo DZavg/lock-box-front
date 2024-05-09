9421715f412393ef041334261249d86bcbb597ed
DZavg <Danilkaqaz123@gmail.com>
2024-05-09 20:22:03 +0700

    feat(httpClient): create http client

---
+++ src/shared/api/client/httpClient.ts
@@ -0,0 +1,29 @@
+import axios, { type CreateAxiosDefaults } from 'axios'
+
+const createHttpClient = (config: CreateAxiosDefaults) => {
+	const httpClient = axios.create(config)
+
+	httpClient.interceptors.request.use(
+		function (config) {
+			return config
+		},
+		function (error) {
+			return Promise.reject(error)
+		},
+	)
+
+	axios.interceptors.response.use(
+		function (response) {
+			return response
+		},
+		function (error) {
+			return Promise.reject(error)
+		},
+	)
+
+	return httpClient
+}
+
+export const httpClient = createHttpClient({
+	baseURL: import.meta.env.VITE_API_URL,
+})
