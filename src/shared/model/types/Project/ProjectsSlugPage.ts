62e998bbcb071edc9ea068a1588231c1cddd88ee
DZavg <Danilkaqaz123@gmail.com>
2024-05-09 20:44:55 +0700

    feat(ProjectsSlugPage): create interface

---
+++ src/shared/model/types/Project/ProjectsSlugPage.ts
@@ -0,0 +1,6 @@
+import { type Project } from '@/shared/model/types/Project/Project'
+import { type Access } from '@/shared/model/types/Access/Access'
+
+export interface ProjectsSlugPage extends Project {
+	access: Access
+}
