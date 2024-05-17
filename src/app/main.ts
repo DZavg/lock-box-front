703ec86e3e4cc1c8122ca020ef6eefb39f3ff73c
Данил <d.zavgorodnij@meta.studio>
2024-05-17 19:08:16 +0700

    feat(Vue3Toastify): link lib

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import checkScreen from '@/app/plugins/checkScreen'
@@ -11,6 +12,16 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(checkScreen)
 app.use(router)
+app.use(Vue3Toastify, {
+	position: 'top-center',
+	autoClose: 30000000,
+	multiple: false,
+	icon: true,
+	hideProgressBar: true,
+	closeButton: false,
+	theme: 'colored',
+	dangerouslyHTMLString: true,
+} as ToastContainerOptions)
 
 app.directive('clickOutside', clickOutside)
 
