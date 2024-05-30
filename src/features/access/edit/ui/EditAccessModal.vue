c329ce891c82c2534adb4b1c46e3776636ac08a6
Данил <d.zavgorodnij@meta.studio>
2024-05-30 21:47:02 +0700

    feat(EditAccessModal): create props and use useEditAccessForm composable

--- src/features/access/edit/ui/EditAccessModal.vue
+++ src/features/access/edit/ui/EditAccessModal.vue
@@ -2,10 +2,15 @@
 	<BaseModal class="edit-access-modal" title="Редактировать доступ">
 		<BaseForm class="edit-access-modal__form">
 			<InputList>
-				<BaseInput label="Адрес" name="address" placeholder="Поле ввода" />
-				<BaseInput label="Логин" name="login" placeholder="Поле ввода" />
-				<PasswordInput />
-				<BaseSelect :options="[{ id: '0', title: 'SSH' }]" label="Тип доступа" name="type" />
+				<BaseInput label="Адрес" name="address" placeholder="Поле ввода" v-model="form.origin" />
+				<BaseInput label="Логин" name="login" placeholder="Поле ввода" v-model="form.login" />
+				<PasswordInput v-model="form.password" />
+				<BaseSelect
+					:options="[{ id: '0', title: 'SSH' }]"
+					label="Тип доступа"
+					name="type"
+					v-model="form.type"
+				/>
 			</InputList>
 		</BaseForm>
 		<BaseButton class="edit-access-modal__button">Сохранить изменения</BaseButton>
@@ -20,6 +25,24 @@ import PasswordInput from '@/shared/ui/Input/PasswordInput.vue'
 import BaseSelect from '@/shared/ui/Select/BaseSelect.vue'
 import BaseForm from '@/shared/ui/Form/BaseForm.vue'
 import InputList from '@/shared/ui/Input/InputList.vue'
+import type { Access } from '@/shared/model/types/Access/Access'
+import useEditAccessForm from '@/features/access/edit/composable/useEditAccessForm'
+
+interface Props {
+	access: Access
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	access: () => ({
+		id: '',
+		origin: '',
+		login: '',
+		type: '',
+	}),
+})
+
+const { form, fillForm } = useEditAccessForm()
+fillForm(props.access)
 </script>
 
 <style lang="scss" scoped>
