4cdec5f8af714495f1b5d01612a836674a9035bd
Данил <d.zavgorodnij@meta.studio>
2024-06-04 20:15:23 +0700

    feat(CloseButtonIcon): add size prop

--- src/shared/ui/Button/CloseButtonIcon.vue
+++ src/shared/ui/Button/CloseButtonIcon.vue
@@ -1,10 +1,17 @@
 <template>
-	<BaseButtonIcon :icon="iconClose" />
+	<BaseButtonIcon :icon="iconClose" :size="size" />
 </template>
 
 <script lang="ts" setup>
 import iconClose from '@/shared/images/svg/icon-close.svg'
 import BaseButtonIcon from '@/shared/ui/Button/BaseButtonIcon.vue'
+import { IconSize } from '@/shared/model/types/Icon/IconSize'
+
+interface Props {
+	size?: IconSize
+}
+
+defineProps<Props>()
 </script>
 
 <style lang="scss" scoped></style>
