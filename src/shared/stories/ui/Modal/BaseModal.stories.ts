7a5cec6cebd78784eb7c324bddb7cd16238493b2
Данил <d.zavgorodnij@meta.studio>
2024-06-04 19:41:12 +0700

    refactor(BaseModal.stories): use slots field

--- src/shared/stories/ui/Modal/BaseModal.stories.ts
+++ src/shared/stories/ui/Modal/BaseModal.stories.ts
@@ -9,6 +9,11 @@ const meta: Meta<typeof BaseModal> = {
 			type: 'figma',
 			url: 'https://www.figma.com/file/JrFyfzV8shSxNFSTQdqb4J/password-storage?type=design&node-id=27-1114&mode=design&t=Tv31JZ2gceHvRaZa-0',
 		},
+		slots: {
+			default: {
+				template: `modal`,
+			},
+		},
 	},
 }
 
@@ -16,16 +21,7 @@ export default meta
 type Story = StoryObj<typeof BaseModal>
 
 export const Default: Story = {
-	args: {},
-	render: (args: any) => ({
-		components: { BaseModal },
-		setup() {
-			return { args }
-		},
-		template: `
-			<BaseModal v-bind="args">
-				<div>modal</div>
-			</BaseModal>
-		`,
-	}),
+	args: {
+		title: 'Modal',
+	},
 }
