a3db4aab9e6fd0a44589db785680c9a2f2a09017
Данил <d.zavgorodnij@meta.studio>
2024-06-11 20:20:23 +0700

    feat(EyeLinkIcon): rename prop iconSize to size

--- src/shared/ui/Link/EyeLinkIcon.vue
+++ src/shared/ui/Link/EyeLinkIcon.vue
@@ -1,17 +1,20 @@
 <template>
-	<BaseLinkIcon :icon="iconEyeOpen" :link="link" />
+	<BaseLinkIcon :icon="iconEyeOpen" :link="link" :size="size" />
 </template>
 
 <script lang="ts" setup>
 import iconEyeOpen from '@/shared/images/svg/icon-eye-open.svg'
 import BaseLinkIcon from '@/shared/ui/Link/BaseLinkIcon.vue'
+import { IconSize } from '@/shared/model/types/Icon/IconSize'
 
 interface Props {
 	link: string | object
+	size?: IconSize
 }
 
 withDefaults(defineProps<Props>(), {
 	link: '',
+	size: IconSize.M,
 })
 </script>
 
