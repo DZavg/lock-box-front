91b8708df389e30ae9a6d4158ee353cb9aa682a7
Данил <d.zavgorodnij@meta.studio>
2024-06-11 17:33:01 +0700

    feat(BaseOpacityTransition): create component

---
+++ src/shared/ui/Transition/BaseOpacityTransition.vue
@@ -0,0 +1,19 @@
+<template>
+	<transition name="opacity-transition">
+		<slot></slot>
+	</transition>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.opacity-transition-enter-active,
+.opacity-transition-leave-active {
+	transition: opacity 0.2s ease;
+}
+
+.opacity-transition-enter-from,
+.opacity-transition-leave-to {
+	opacity: 0;
+}
+</style>
