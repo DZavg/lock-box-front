8bf574b1a8c8f93eae9a9f2055d2d4016118076d
Данил <d.zavgorodnij@meta.studio>
2024-06-12 18:05:42 +0700

    feat(ProjectsPage): use ProjectsTable component

--- src/pages/project/ui/ProjectsPage.vue
+++ src/pages/project/ui/ProjectsPage.vue
@@ -3,13 +3,17 @@
 		<template #head>
 			<ProjectsActions />
 		</template>
-		<template #default> </template>
+		<template #default>
+			<ProjectsTable :projects="getProjects()" />
+		</template>
 	</BaseInternalPage>
 </template>
 
 <script lang="ts" setup>
 import BaseInternalPage from '@/shared/ui/InternalPage/BaseInternalPage.vue'
 import ProjectsActions from '@/features/projects/ui/ProjectsActions.vue'
+import getProjects from '@/shared/stories/data/projects'
+import ProjectsTable from '@/widgets/projects/ui/ProjectsTable.vue'
 </script>
 
 <style lang="scss" scoped></style>
