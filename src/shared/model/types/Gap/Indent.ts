3f941208286677a82578f84a159f35938d143cf6
Данил <d.zavgorodnij@meta.studio>
2024-06-12 21:58:05 +0700

    feat(Indent): create interface

---
+++ src/shared/model/types/Gap/Indent.ts
@@ -0,0 +1,11 @@
+export enum Indent {
+	TwoXs = 'var(--indent-2xs)',
+	Xs = 'var(--indent-2xs)',
+	S = 'var(--indent-s)',
+	M = 'var(--indent-m)',
+	L = 'var(--indent-l)',
+	Xl = 'var(--indent-xl)',
+	TwoXl = 'var(--indent-2xl)',
+	ThreeXl = 'var(--indent-3xl)',
+	FourXl = 'var(--indent-4xl)',
+}
