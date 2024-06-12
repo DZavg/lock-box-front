ffad4863e64935ff9fd2ffd334c14219f8b95673
Данил <d.zavgorodnij@meta.studio>
2024-06-12 22:35:05 +0700

    feat(BaseTwoColumnGrid): create component

---
+++ src/shared/ui/Grid/BaseTwoColumnGrid.vue
@@ -0,0 +1,44 @@
+<template>
+	<BaseColumnGrid class="two-column-grid" v-bind="props">
+		<slot></slot>
+	</BaseColumnGrid>
+</template>
+
+<script setup lang="ts">
+import BaseColumnGrid from '@/shared/ui/Grid/BaseColumnGrid.vue'
+import { Indent } from '@/shared/model/types/Gap/Indent'
+import type { Gap } from '@/shared/model/types/Gap/Gap'
+
+interface Props {
+	desktopGap?: Gap
+	tabletGap?: Gap
+	mobileGap?: Gap
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	desktopGap: () => ({
+		row: Indent.L,
+		column: Indent.L,
+	}),
+	tabletGap: () => ({
+		row: Indent.M,
+		column: Indent.M,
+	}),
+	mobileGap: () => ({
+		row: Indent.S,
+		column: Indent.S,
+	}),
+})
+</script>
+
+<!--TODO: create story-->
+
+<style lang="scss" scoped>
+.two-column-grid {
+	grid-template-columns: repeat(2, 1fr);
+
+	@media screen and (max-width: 768px) {
+		grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
+	}
+}
+</style>
