5440720292e53d0057766967f516caf45eb3fc50
Данил <d.zavgorodnij@meta.studio>
2024-06-12 19:01:59 +0700

    feat(AccessesTable): create component and story

---
+++ src/widgets/accesses/stories/AccessesTable.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import AccessesTable from '@/widgets/accesses/ui/AccessesTable.vue'
+import getAccesses from '@/shared/stories/data/access'
+
+const meta: Meta<typeof AccessesTable> = {
+	component: AccessesTable,
+	tags: ['autodocs'],
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/design/JrFyfzV8shSxNFSTQdqb4J/password-storage?node-id=1-5&t=TQF51cOOVifctsZn-0',
+		},
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof AccessesTable>
+
+export const Default: Story = {
+	args: {
+		accesses: getAccesses(),
+	},
+}
