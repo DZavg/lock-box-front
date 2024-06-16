e41c11092dc2995b7216c77e31599c295dcf986b
DZavg <Danilkaqaz123@gmail.com>
2024-06-16 22:51:18 +0700

    feat(ProjectsPage): add SearchNotFound component

--- src/views/project/ProjectsPage.vue
+++ src/views/project/ProjectsPage.vue
@@ -7,7 +7,11 @@
       <ProjectsActions />
     </template>
     <template #default>
-      <ProjectsTable :projects="projectsPageData.projects" />
+      <ProjectsTable
+        v-if="true"
+        :projects="projectsPageData.projects"
+      />
+      <SearchNotFound v-else />
     </template>
   </BaseInternalPage>
 </template>
@@ -17,6 +21,7 @@ import BaseInternalPage from '@/components/ui/InternalPage/BaseInternalPage.vue'
 import ProjectsActions from '@/components/Projects/ProjectsActions.vue'
 import ProjectsTable from '@/components/Projects/ProjectsTable.vue'
 import { projectsPageData } from '@/global/mock/pages/projects/ProjectsPageData'
+import SearchNotFound from '@/components/Search/SearchNotFound.vue'
 </script>
 
 <style lang="scss" scoped></style>
