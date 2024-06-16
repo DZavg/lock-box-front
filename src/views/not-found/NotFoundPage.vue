5035d3383c91975ce0ea8dd9e4be107bd00a62f1
DZavg <Danilkaqaz123@gmail.com>
2024-06-16 23:13:20 +0700

    feat(NotFoundPage): use BaseImage component

--- src/views/not-found/NotFoundPage.vue
+++ src/views/not-found/NotFoundPage.vue
@@ -1,23 +1,23 @@
 <template>
   <ContentCentering class="not-found-page container-s">
-    <div class="not-found-page__wrapper">
-      <img
-        alt="Не найдено"
-        class="not-found-page__image"
-        height="176"
-        src="https://placehold.co/468x176/orange/white"
-        width="468"
-      >
-      <h1 class="not-found-page__title">
-        Похоже мы не можем найти нужную вам страницу
-      </h1>
-      <BaseLinkButton
-        :link="RouteName.Projects"
-        class="not-found-page__link"
-      >
-        На главную
-      </BaseLinkButton>
-    </div>
+    <BaseImage
+      class="not-found-page__image"
+      :image="{
+        src: 'https://placehold.co/468x176/orange/white',
+        alt: 'Не найдено',
+        title: 'Не найдено',
+      }"
+      :image-options="{ width: '468', height: '176' }"
+    />
+    <h1 class="not-found-page__title">
+      Похоже мы не можем найти нужную вам страницу
+    </h1>
+    <BaseLinkButton
+      :link="RouteName.Projects"
+      class="not-found-page__link"
+    >
+      На главную
+    </BaseLinkButton>
   </ContentCentering>
 </template>
 
@@ -25,14 +25,13 @@
 import BaseLinkButton from '@/components/ui/Link/BaseLinkButton.vue'
 import { RouteName } from '@/router/RouteName'
 import ContentCentering from '@/components/ui/Content/ContentCentering.vue'
+import BaseImage from '@/components/ui/Image/BaseImage.vue'
 </script>
 
 <style lang="scss" scoped>
 .not-found-page {
 	&__image {
-		width: 100%;
 		max-width: 468px;
-		height: auto;
 		margin-bottom: $indent-xl;
 
 		@media screen and (max-width: 768px) {
