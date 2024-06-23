738f5436db807e3df87a202cb21169c0933efda4
DZavg <Danilkaqaz123@gmail.com>
2024-06-23 21:25:12 +0700

    feat(EmailInput): add prop "error"

--- src/components/ui/Input/EmailInput.vue
+++ src/components/ui/Input/EmailInput.vue
@@ -2,12 +2,21 @@
   <BaseInput
     label="Электронная почта"
     name="email"
+    :error="error"
     placeholder="ivan@gmail.com"
   />
 </template>
 
 <script lang="ts" setup>
 import BaseInput from '@/components/ui/Input/BaseInput.vue'
+
+interface Props {
+	error?: string
+}
+
+withDefaults(defineProps<Props>(), {
+	error: '',
+})
 </script>
 
 <style lang="scss" scoped></style>
