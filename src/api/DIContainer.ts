6f9d4f455548718013fa06b178ab24256c5c9145
DZavg <Danilkaqaz123@gmail.com>
2024-06-23 21:50:16 +0700

    feat(DIContainer): bind AuthRepository and Login

--- src/api/DIContainer.ts
+++ src/api/DIContainer.ts
@@ -9,6 +9,8 @@ import {
 import { type GetOneById, GetOneByIdImpl } from '@/api/project/usecase/getOneById'
 import { identifiers } from '@/api/constants/identifiers'
 import { type GetAll, GetAllImpl } from '@/api/project/usecase/getAll'
+import { type Login, LoginImpl } from '@/api/auth/usecase/login'
+import { type AuthRepository, AuthRepositoryImpl } from '@/api/auth/repository/AuthRepository'
 
 const container: Container = new Container()
 
@@ -17,5 +19,8 @@ container.bind<AxiosInstance>(identifiers.httpClient).toConstantValue(httpClient
 container.bind<ProjectRepository>(identifiers.projectRepository).to(ProjectRepositoryImpl)
 container.bind<GetOneById>(identifiers.getOneProjectById).to(GetOneByIdImpl)
 container.bind<GetAll>(identifiers.getAllProjects).to(GetAllImpl)
+
+container.bind<AuthRepository>(identifiers.authRepository).to(AuthRepositoryImpl)
+container.bind<Login>(identifiers.login).to(LoginImpl)
 
 export { container }
