090b06f9ef05b4c789353ba7df7f360a38aca64e
DZavg <Danilkaqaz123@gmail.com>
2024-06-23 21:28:04 +0700

    feat(ProjectsPage): add get data on mounted

--- src/views/project/ProjectsPage.vue
+++ src/views/project/ProjectsPage.vue
@@ -9,7 +9,7 @@
     <template #default>
       <ProjectsTable
         v-if="true"
-        :projects="projectsPageData.projects"
+        :projects="projectPage?.projects"
       />
       <SearchNotFound v-else />
     </template>
@@ -20,8 +20,18 @@
 import BaseInternalPage from '@/components/ui/InternalPage/BaseInternalPage.vue'
 import ProjectsActions from '@/components/Projects/ProjectsActions.vue'
 import ProjectsTable from '@/components/Projects/ProjectsTable.vue'
-import { projectsPageData } from '@/global/mock/pages/projects/ProjectsPageData'
 import SearchNotFound from '@/components/Search/SearchNotFound.vue'
+import { onMounted } from 'vue'
+import { useProjectStore } from '@/stores/project'
+import useRequest from '@/composables/useRequest'
+
+const projectStore = useProjectStore()
+const { getAll } = projectStore
+const { data: projectPage, execute } = useRequest()
+
+onMounted(async () => {
+	await execute(getAll)
+})
 </script>
 
 <style lang="scss" scoped></style>
