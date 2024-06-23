4426fb33354b7915d6ba2c1cc44369f7f1d88092
DZavg <Danilkaqaz123@gmail.com>
2024-06-23 21:47:13 +0700

    feat(identifiers): add authRepository and login fields

--- src/api/constants/identifiers.ts
+++ src/api/constants/identifiers.ts
@@ -1,6 +1,10 @@
 export const identifiers = {
 	httpClient: Symbol.for('HttpClient'),
+
 	projectRepository: Symbol.for('ProjectRepository'),
 	getOneProjectById: Symbol.for('GetOneProjectById'),
 	getAllProjects: Symbol.for('GetAllProjects'),
+
+	authRepository: Symbol.for('AuthRepository'),
+	login: Symbol.for('Login'),
 }
