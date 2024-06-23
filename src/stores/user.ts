da226e8d738e27e71372ce5aa7053f4824ca94cd
DZavg <Danilkaqaz123@gmail.com>
2024-06-23 21:50:55 +0700

    feat(user): create store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,4 +1,10 @@
 import { defineStore } from 'pinia'
+import { container } from '@/api/DIContainer'
+import { identifiers } from '@/api/constants/identifiers'
+import type { LoginDto } from '@/api/auth/dto/login.dto'
+import type { Login } from '@/api/auth/usecase/login'
+
+const login = container.get<Login>(identifiers.login)
 
 export const useUserStore = defineStore('user', {
 	state: () => {
@@ -8,7 +14,11 @@ export const useUserStore = defineStore('user', {
 		}
 	},
 
-	actions: {},
+	actions: {
+		async login(data: LoginDto) {
+			return await login.execute(data)
+		},
+	},
 
 	getters: {
 		getIsAuthorized(): boolean {
