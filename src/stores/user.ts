2a739b702bd060f465b9b6cf28472e6dccc8edd8
DZavg <Danilkaqaz123@gmail.com>
2024-06-24 19:52:01 +0700

    feat(user): create singIn and singOut functions

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,20 +3,40 @@ import { container } from '@/api/DIContainer'
 import { identifiers } from '@/api/constants/identifiers'
 import type { LoginDto } from '@/api/auth/dto/login.dto'
 import type { Login } from '@/api/auth/usecase/login'
+import type { SetTokens } from '@/api/tokens/usecase/setTokens'
+import type { Tokens } from '@/api/tokens/entity/Tokens'
+import type { RemoveTokens } from '@/api/tokens/usecase/removeTokens'
 
 const login = container.get<Login>(identifiers.login)
+const setTokens = container.get<SetTokens>(identifiers.setTokens)
+const removeTokens = container.get<RemoveTokens>(identifiers.removeTokens)
 
 export const useUserStore = defineStore('user', {
 	state: () => {
 		return {
 			user: null,
-			isAuthorized: true,
+			isAuthorized: false,
 		}
 	},
 
 	actions: {
 		async login(data: LoginDto) {
-			return await login.execute(data)
+			const response = await login.execute(data)
+			const { access_token, refresh_token } = response
+			this.singIn({ access_token, refresh_token })
+			return response
+		},
+
+		singIn(tokens: Tokens) {
+			this.isAuthorized = true
+			if (tokens.access_token && tokens.refresh_token) {
+				setTokens.execute(tokens)
+			}
+		},
+
+		singOut() {
+			this.isAuthorized = false
+			removeTokens.execute()
 		},
 	},
 
