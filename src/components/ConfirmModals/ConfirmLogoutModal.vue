fe943b70685c992374efbedfab2ca022dffd3876
DZavg <Danilkaqaz123@gmail.com>
2024-06-25 21:19:37 +0700

    feat(ConfirmLogoutModal): add logout func

--- src/components/ConfirmModals/ConfirmLogoutModal.vue
+++ src/components/ConfirmModals/ConfirmLogoutModal.vue
@@ -2,11 +2,29 @@
   <ConfirmActionModal
     title="Вы действительно хотите выйти из аккаунта?"
     button-confirm-text="Выйти"
+    @on-close="$emit('onClose')"
+    @on-confirm="logout"
   />
 </template>
 
 <script setup lang="ts">
 import ConfirmActionModal from '@/components/ConfirmModals/ConfirmActionModal.vue'
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import { RouteName } from '@/router/RouteName'
+
+const router = useRouter()
+const authStore = useAuthStore()
+const { logout: fetchLogout } = authStore
+
+const logout = async () => {
+	await fetchLogout()
+	await router.push(RouteName.Auth)
+}
+
+defineEmits<{
+	(e: 'onClose'): void
+}>()
 </script>
 
 <style scoped lang="scss"></style>
