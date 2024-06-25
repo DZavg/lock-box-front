9f2a7f6fcbd1d6d8f8799c914e603184d346662d
DZavg <Danilkaqaz123@gmail.com>
2024-06-25 20:53:32 +0700

    feat(auth): add func "logout"

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,8 +7,10 @@ import type { SetTokens } from '@/api/tokens/usecase/setTokens'
 import type { Tokens } from '@/api/tokens/entity/Tokens'
 import type { RemoveTokens } from '@/api/tokens/usecase/removeTokens'
 import type { GetTokens } from '@/api/tokens/usecase/getTokens'
+import type { Logout } from '@/api/auth/usecase/logout'
 
 const login = container.get<Login>(identifiers.login)
+const logout = container.get<Logout>(identifiers.logout)
 const setTokens = container.get<SetTokens>(identifiers.setTokens)
 const getTokens = container.get<GetTokens>(identifiers.getTokens)
 const removeTokens = container.get<RemoveTokens>(identifiers.removeTokens)
@@ -25,6 +27,12 @@ export const useAuthStore = defineStore('auth', {
 			const response = await login.execute(data)
 			const { access_token, refresh_token } = response
 			this.singIn({ access_token, refresh_token })
+			return response
+		},
+
+		async logout() {
+			const response = await logout.execute()
+			this.singOut()
 			return response
 		},
 
