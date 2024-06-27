fd6fba548afd95424320563db8392bb21feec112
DZavg <Danilkaqaz123@gmail.com>
2024-06-27 19:45:37 +0700

    feat(ProjectModal): add props and emit

--- src/components/Projects/ProjectModal.vue
+++ src/components/Projects/ProjectModal.vue
@@ -3,12 +3,16 @@
     class="project-modal"
     :title="title"
   >
-    <BaseForm class="project-modal__form">
+    <BaseForm
+      class="project-modal__form"
+      @submit="$emit('onSubmit', form)"
+    >
       <InputList>
         <BaseInput
           v-model="form.title"
           label="Название проекта"
           name="title"
+          :error="errors.title"
           placeholder="Поле ввода"
         />
         <BaseInput
@@ -16,10 +20,15 @@
           label="Домен"
           name="domain"
           placeholder="Поле ввода"
+          :error="errors.domain"
         />
       </InputList>
     </BaseForm>
-    <BaseButton class="project-modal__button">
+    <BaseButton
+      class="project-modal__button"
+      :loading="loading"
+      @on-click="$emit('onSubmit', form)"
+    >
       Сохранить изменения
     </BaseButton>
   </BaseModal>
@@ -33,10 +42,13 @@ import BaseForm from '@/components/ui/Form/BaseForm.vue'
 import InputList from '@/components/ui/Input/InputList.vue'
 import type { Project } from '@/api/project/entity/Project'
 import useProjectForm from '@/composables/forms/useProjectForm'
+import type { Error } from '@/global/types/api/error/Error'
 
 interface Props {
 	project?: Project
 	title?: string
+	errors?: Error
+	loading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -46,9 +58,15 @@ const props = withDefaults(defineProps<Props>(), {
 		domain: '',
 	}),
 	title: 'Редактировать проект',
+	loading: false,
+	errors: () => ({}),
 })
 
 const { form } = useProjectForm(props.project)
+
+defineEmits<{
+	(e: 'onSubmit', form: Project): void
+}>()
 </script>
 
 <style lang="scss" scoped>
