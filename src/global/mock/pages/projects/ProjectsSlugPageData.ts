99a503d3fdab35168031edaf039bfafe44c46651
DZavg <Danilkaqaz123@gmail.com>
2024-07-29 12:45:37 +0300

    feat(ProjectsSlugPageData): delete mock data for page

--- src/global/mock/pages/projects/ProjectsSlugPageData.ts
+++
@@ -1,8 +0,0 @@
-import type { ProjectsSlugPage } from '@/api/project/entity/ProjectsSlugPage'
-import getAccesses from '@/global/mock/access'
-import getProjects from '@/global/mock/projects'
-
-export const projectsSlugPageData: ProjectsSlugPage = {
-	accesses: getAccesses(),
-	project: getProjects()[0],
-}
