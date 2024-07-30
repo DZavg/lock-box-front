57473c38eb12ff768930552403c69a690cab6aca
DZavg <Danilkaqaz123@gmail.com>
2024-07-30 18:36:17 +0300

    feat(BaseLinkIcon): remove external link and emit

--- src/components/ui/Link/BaseLinkIcon.vue
+++ src/components/ui/Link/BaseLinkIcon.vue
@@ -1,18 +1,5 @@
 <template>
-  <a
-    v-if="isExternalLink"
-    :href="'//' + link"
-    class="link-icon"
-    target="_blank"
-    v-bind="$attrs"
-  >
-    <BaseIcon
-      :icon="icon"
-      :size="size"
-    />
-  </a>
   <RouterLink
-    v-else
     :target="target"
     :to="link"
     class="link-icon"
@@ -28,7 +15,6 @@
 <script lang="ts" setup>
 import BaseIcon from '@/components/ui/Icon/BaseIcon.vue'
 import { type HtmlLinkTarget } from '@/global/types/ui/link/HtmlLinkTarget'
-import useLink from '@/composables/useLink'
 import { IconSize } from '@/global/types/ui/icon/IconSize'
 
 interface Props {
@@ -38,16 +24,12 @@ interface Props {
 	size?: IconSize
 }
 
-const props = withDefaults(defineProps<Props>(), {
+withDefaults(defineProps<Props>(), {
 	link: '',
 	icon: '',
 	target: '_self',
 	size: IconSize.M,
 })
-
-const { isExternalLink } = useLink(props.link)
-
-defineEmits<{ (e: 'onClick'): void }>()
 </script>
 
 <style lang="scss" scoped>
--- src/composables/useLink.ts
+++
@@ -1,9 +0,0 @@
-import { computed } from 'vue'
-
-export default function (link: string | object) {
-	const isExternalLink = computed(() => {
-		return typeof link === 'string'
-	})
-
-	return { isExternalLink }
-}
