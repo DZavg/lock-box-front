2ee9af5f561a443d01bbc0486b7dc654ef79cd6a
DZavg <Danilkaqaz123@gmail.com>
2024-08-02 19:01:31 +0300

    feat(LoadingIcon.stories): add args "size" and "color"

--- src/stories/ui/Loading/LoadingIcon.stories.ts
+++ src/stories/ui/Loading/LoadingIcon.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import LoadingIcon from '@/components/ui/Loading/LoadingIcon.vue'
+import { IconSize } from '@/global/types/ui/icon/IconSize'
+import { Color } from '@/global/types/ui/color/Color'
 
 const meta: Meta<typeof LoadingIcon> = {
 	component: LoadingIcon,
@@ -9,4 +11,9 @@ const meta: Meta<typeof LoadingIcon> = {
 export default meta
 type Story = StoryObj<typeof LoadingIcon>
 
-export const Default: Story = {}
+export const Default: Story = {
+	args: {
+		size: IconSize.ThreeXL,
+		color: Color.White,
+	},
+}
