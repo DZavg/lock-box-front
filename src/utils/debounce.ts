2d9e1f2c46e319b79fb0d1041684fd31a29ade75
DZavg <Danilkaqaz123@gmail.com>
2024-08-07 19:15:53 +0300

    feat(debounce): use apply

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,13 +1,11 @@
 export default function debounce(func: Function, delay: number = 300) {
 	let timeoutId: ReturnType<typeof setTimeout> | null = null
 
-	return function (...args: any) {
+	return function (this: unknown, ...args: any) {
 		if (timeoutId) {
 			clearTimeout(timeoutId)
 		}
 
-		timeoutId = setTimeout(function () {
-			func(args)
-		}, delay)
+		timeoutId = setTimeout(() => func.apply(this, args), delay)
 	}
 }
