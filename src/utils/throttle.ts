7d08f142e65d90d339a2b0303464c88f217b4191
DZavg <Danilkaqaz123@gmail.com>
2024-08-08 22:02:15 +0300

    feat(throttle): add func "apply"

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,10 +1,10 @@
 export default function (func: Function, delay: number = 200) {
 	let timeoutId: ReturnType<typeof setTimeout> | null = null
 
-	return function (...args: any) {
+	return function (this: unknown, ...args: any) {
 		if (timeoutId) return
 		timeoutId = setTimeout(() => {
-			func(...args)
+			func.apply(this, args)
 
 			clearTimeout(timeoutId!)
 			timeoutId = null
