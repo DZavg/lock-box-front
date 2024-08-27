cde92b6abb3254f1a53a130ee4a8b7e9e54c072a
DZavg <Danilkaqaz123@gmail.com>
2024-08-27 23:52:38 +0300

    feat(useRequest): add type for variable "errors"

--- src/composables/useRequest.ts
+++ src/composables/useRequest.ts
@@ -3,6 +3,7 @@ import { toast } from 'vue3-toastify'
 import errorsTransform from '@/utils/errorsTransform'
 import { HttpStatus } from '@/utils/httpStatus'
 import { useLoaderStore } from '@/stores/loader'
+import type { Error } from '@/global/types/api/error/Error'
 
 export default function () {
 	const loaderStore = useLoaderStore()
@@ -10,7 +11,7 @@ export default function () {
 
 	const data: Ref<any> = ref()
 	const isLoading: Ref<boolean> = ref(false)
-	const errors: Ref<any> = ref({})
+	const errors: Ref<Error> = ref({})
 
 	const execute = async (callback: Function = () => {}) => {
 		if (isLoading.value) {
